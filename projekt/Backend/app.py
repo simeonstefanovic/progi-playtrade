df0d8293d698af78fedef693bc6b1be6316e87a0
batonkal <batonkal13@gmail.com>
2025-11-13 13:48:12 +0100

    Popravljeni CORS errors

--- projekt/Backend/app.py
+++ projekt/Backend/app.py
@@ -1,20 +1,38 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from config import Config
 from database import db
 from routes.auth import auth
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../Frontend/build", static_url_path="/")
 app.config.from_object(Config)
 
-CORS(app)
+# CORS samo za development, debug
+if app.config.get("DEBUG", False):
+    CORS(app)
+
 db.init_app(app)
-JWTManager(app)
+jwt = JWTManager(app)
 bcrypt = Bcrypt(app)
 
 app.register_blueprint(auth, url_prefix="/api")
+
+
+# Spajanje s React frontendom
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def server_react(path):
+    build_dir = app.static_folder
+    file_path = os.path.join(build_dir, path)
+
+    if path != "" and os.path.exists(file_path):
+        return send_from_directory(build_dir, path) 
+    else:
+        return send_from_directory(build_dir, "index.html") 
+
 
 with app.app_context():
     db.create_all()
--- projekt/Frontend/src/pages/Login.jsx
+++ projekt/Frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function LoginPage() {
   function handleSubmit(e) {
     e.preventDefault();
 
-    fetch("http://127.0.0.1:5000/api/login", {
+    fetch("/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password })
